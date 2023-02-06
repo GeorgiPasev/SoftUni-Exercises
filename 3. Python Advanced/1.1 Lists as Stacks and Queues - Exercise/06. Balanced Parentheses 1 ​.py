8363b287ee81a55392cea72e869f7f478867300a
G <gw2009@windowslive.com>
2023-02-06 19:57:57 +0200

    upload 6

---
+++ 3. Python Advanced/1.1 Lists as Stacks and Queues - Exercise/06. Balanced Parentheses 1 ​.py
@@ -0,0 +1,32 @@
+parentheses = input()
+stack = []
+result = True
+
+for p in parentheses:
+    if p in ['{', '[', '(']:
+        stack.append(p)
+    
+    elif p == '}':
+        if stack and stack[-1] == '{':
+            stack.pop()
+        else:
+            result = False
+            break
+    
+    elif p == ']':
+        if stack and stack[-1] == '[':
+            stack.pop()
+        else:
+            result = False
+            break
+    
+    elif p == ')':
+        if stack and stack[-1] == '(':
+            stack.pop()
+        else:
+            result = False
+            break
+
+        
+
+print('YES' if result else 'NO')
